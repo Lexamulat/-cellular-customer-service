4abd537da8983b3dc26532d34921220ec3a772b5
lexamulat <lexamulat@mail.ru>
2018-04-11 23:15:11 +0300

    1

--- hashtable.h
+++ hashtable.h
@@ -9,10 +9,11 @@ public:
 	~hashtable();
 	bool IsTableFull();
 	void DelfromTable();
-	void tablesearch(bool callWithString = 0,std::string input="_" ,bool CallFromDel = 0);
+	void tablesearch(bool CallFromDel = 0);
 	void show();
 	int hashFunc(std::string simnum);
 	void preAdd();
+	bool TableSearchWithInputString(std::string input);
 	void addInTable(std::shared_ptr<sim> obj);
 private:
 	 
